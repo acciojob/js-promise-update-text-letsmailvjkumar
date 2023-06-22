649d56ef1a8986084cc4743db26027ec659612e8
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-22 15:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateElementTextWithPromise() {
+return new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("Hello, world!")
+	},1000)
+})
+}
+
+const outputElement = document.getElementById("output")
+
+updateElementTextWithPromise().then((result)=>{
+	outputElement.innerText=result;
+}
